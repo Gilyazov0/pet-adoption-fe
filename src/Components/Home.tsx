500117ef79ffe65d4ccce4473d196ff60f25201a
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-12-22 17:56:02 +0200

    home page reworked

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,15 +1,18 @@
-import { Pets } from "../PetsDataSet";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PetCardsList from "./PetCardsList";
-import PetProps from "../Types/PetProps";
 import "../style/Home.css";
+import { useContext } from "react";
+import { UserContext } from "../App";
 function Home() {
+  const { user } = useContext(UserContext);
   return (
     <div className="App">
       <div className="home">
         <div className="title">
-          Standing up for animals since... just about to start
+          {user
+            ? `Welcome back, ${user.firstName} ${user.lastName}`
+            : "Standing up for animals since... just about to start"}
         </div>
+
         <img className="home-img" src="./home3.png" alt="" />
       </div>
     </div>
